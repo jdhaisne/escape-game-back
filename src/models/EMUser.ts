a552356fbd4cd71bb6e6d5c55f902174ce877aa0
LilyP87 <apavilla@outlook.fr>
2023-06-27 13:50:21 +0200

    new model users

--- src/models/EMUser.ts
+++ src/models/EMUser.ts
@@ -1,10 +1,11 @@
-import mongoose from "mongoose";
-
+const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
+    id: mongoose.ObjectId,
     name: String,
+    firstName: String,
     password: String,
-    // mail: String,
-    // dateOfBirth: String,
-    // isAdmin: Boolean
+    mail: String,
+    dateOfBirth: String,
+    isAdmin: Boolean
 });
 export const Users = mongoose.model("user", userSchema);
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -10,62 +10,69 @@ const router: Router = express.Router();
 
 // GET USERS : 
 router.get('/', async (req: Request, res: Response) => {
-    try {
-        const users = await Users.find({}).exec();
-        logger.info(users)
-        res.send(users);
-    } 
-    catch (error : any) {
-        logger.error(error)
-        res.status(500).send("Internal Server Error");
-    }
+  try {
+    const users = await Users.find({}).exec();
+    logger.info(users)
+    res.send(users);
+  }
+  catch (error: any) {
+    logger.error(error)
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 
 // PÖST USERS : 
 router.post('/', async (req: Request, res: Response) => {
-    try {
-        const saltRounds = 10;
-        const hash = await bcrypt.hash(req.body.password, saltRounds);
-        await Users.create({ name: req.body.name, password: hash });
-        logger.info(req.body.password);
-        res.send("done");
-    } catch (err : any) {
-        logger.error(err);
-        res.status(500).send("Internal Server Error");
-    }
+  try {
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(req.body.password, saltRounds);
+    await Users.create({
+      name: req.body.name,
+      firstName: req.body.firstName,
+      password: hash,
+      mail: req.body.mail,
+      dateOfBirth: req.body.dateOfBirth,
+      isAdmin: false
+    });
+    logger.info(req.body.password);
+    res.send("done");
+  } catch (err: any) {
+    logger.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 
 // POST LOGIN:
 router.post('/login', async (req: Request, res: Response) => {
-    try {
-      const { name, password } = req.body;
-      const user = await Users.findOne({ name }).exec();
-  
-      if (!user) {
-        return res.status(401).send('User not found');
-      }
-  
-      if (typeof password !== 'string') {
-        return res.status(400).send('Invalid password');
-      }
-  
-      if (!user.password) {
-        return res.status(400).send('No password set for this user');
-      }
-  
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-      if (isPasswordValid) {
-        return res.send('Logged in successfully');
-      } else {
-        return res.status(401).send('Incorrect password');
-      }
-    } catch (err: any) {
-      logger.error(err);
-      return res.status(500).send('Internal Server Error');
+  try {
+    const { mail, password } = req.body;
+    const user = await Users.findOne({ mail }).exec();
+
+    if (!user) {
+      return res.status(401).send('User not found');
     }
+
+    if (typeof password !== 'string') {
+      return res.status(400).send('Invalid password');
+    }
+
+    if (!user.password) {
+      return res.status(400).send('No password set for this user');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      return res.send('Logged in successfully');
+    } else {
+      return res.status(401).send('Incorrect password');
+    }
+  } catch (err: any) {
+    logger.error(err);
+    return res.status(500).send('Internal Server Error');
+  }
 });
 
 
