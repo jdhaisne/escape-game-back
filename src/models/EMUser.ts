2f6a88a2b3808d085b472d7f794e9e752123154e
jdhaisne <julien.dhaisne@gmail.com>
2023-06-27 09:49:26 +0200

    added .env to .gitignore

--- src/models/EMUser.ts
+++ src/models/EMUser.ts
@@ -3,5 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: String,
     password: String,
+    // mail: String,
+    // dateOfBirth: String,
+    // isAdmin: Boolean
 });
 export const Users = mongoose.model("user", userSchema);
