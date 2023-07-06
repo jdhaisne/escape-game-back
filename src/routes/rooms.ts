7bc67a354f2a785dada92032937a81b7ff880c52
SuperCoolNinja <sbpro570@gmail.com>
2023-07-06 12:24:48 +0200

    added new route for rooms

--- src/routes/rooms.ts
+++ src/routes/rooms.ts
@@ -33,4 +33,24 @@ router.get('/:id', async (req: Request, res: Response) => {
     }
 });
 
+
+router.put('/:id/update-availability', async (req: Request, res: Response) => {
+  const roomId = req.params.id;
+  const { availability } = req.body;
+
+  try {
+    const room = await Rooms.findById(roomId);
+
+    if (room) {
+      room.availability = availability;
+      const updatedRoom = await room.save();
+      res.status(200).json(updatedRoom);
+    } else {
+      res.status(404).json({ error: 'Room not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update room availability' });
+  }
+});
+
 export { router as rooms_routes };
